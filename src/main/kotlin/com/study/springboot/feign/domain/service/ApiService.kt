954d96a9866a720c6100de19aebba236f6908b1a
ktj1997 <rlaxowns7916@gmail.com>
2021-08-14 17:47:57 +0900

    feat:Bulkhead 추가

--- src/main/kotlin/com/study/springboot/feign/domain/service/ApiService.kt
+++ src/main/kotlin/com/study/springboot/feign/domain/service/ApiService.kt
@@ -1,32 +1,33 @@
 package com.study.springboot.feign.domain.service
 
 import com.study.springboot.feign.domain.client.SampleFeignClient
-import com.study.springboot.feign.domain.exception.InternalRequestException
 import com.study.springboot.feign.domain.model.SampleApiDto
+import io.github.resilience4j.bulkhead.annotation.Bulkhead
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.stereotype.Service
-import org.springframework.util.MultiValueMap
 
 @Service
 class ApiService(
         val sampleFeignClient: SampleFeignClient
 ) {
     companion object {
-        const val CIRCUIT_BREAKER = "test-circuit"
+        const val RESILIENCE4J = "test-circuit"
     }
 
-    @CircuitBreaker(name = CIRCUIT_BREAKER, fallbackMethod = "fallbackMethod")
+    @Bulkhead(name = RESILIENCE4J)
+    @CircuitBreaker(name = RESILIENCE4J, fallbackMethod = "fallbackMethod")
     fun paramService(title: String, text: String): SampleApiDto {
         return sampleFeignClient.sampleApiRequestParam(title, text)
     }
 
-    @CircuitBreaker(name = CIRCUIT_BREAKER, fallbackMethod = "fallbackMethod")
+    @Bulkhead(name = RESILIENCE4J)
+    @CircuitBreaker(name = RESILIENCE4J, fallbackMethod = "fallbackMethod")
     fun bodyService(dto: SampleApiDto): SampleApiDto {
         return sampleFeignClient.sampleApiRequestBody(dto)
     }
 
-    @CircuitBreaker(name = CIRCUIT_BREAKER, fallbackMethod = "fallbackMethod")
+    @Bulkhead(name = RESILIENCE4J)
+    @CircuitBreaker(name = RESILIENCE4J, fallbackMethod = "fallbackMethod")
     fun pathVariableService(id: Long): SampleApiDto {
         return sampleFeignClient.sampleApiPathVariable(id, "headerValue")
     }
